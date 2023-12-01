d29bc09444f78bcf72d17a2badd6221fff625a1a
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-12-01 19:00:25 -0300

    Update controller.v

--- modules/controlUnit/controller.v
+++ modules/controlUnit/controller.v
@@ -23,12 +23,21 @@ module controller (opCode, branchEn, EXE_CMD, Branch_command, Is_Imm, ST_or_BNE,
         `OP_SLL: begin EXE_CMD <= `EXE_SLL; WB_EN <= 1; end
         `OP_SRA: begin EXE_CMD <= `EXE_SRA; WB_EN <= 1; end
         `OP_SRL: begin EXE_CMD <= `EXE_SRL; WB_EN <= 1; end
+        `OP_MULT: begin EXE_CMD <= `EXE_MULT; WB_EN <= 1; end
+	
         // operations using an immediate value embedded in the instruction
         `OP_ADDI: begin EXE_CMD <= `EXE_ADD; WB_EN <= 1; Is_Imm <= 1; end
         `OP_SUBI: begin EXE_CMD <= `EXE_SUB; WB_EN <= 1; Is_Imm <= 1; end
+        `OP_ADDI: begin EXE_CMD <= `EXE_ADD; WB_EN <= 1; Is_Imm <= 1; end
+        `OP_SUBI: begin EXE_CMD <= `EXE_SUB; WB_EN <= 1; Is_Imm <= 1; end
+        `OP_ANDI: begin EXE_CMD <= `EXE_AND; WB_EN <= 1; Is_Imm <= 1; end
+        `OP_ORI: begin EXE_CMD <= `EXE_OR; WB_EN <= 1; Is_Imm <= 1; end
+        `OP_XORI: begin EXE_CMD <= `EXE_OR; WB_EN <= 1; Is_Imm <= 1; end
+        
         // memory operations
         `OP_LD: begin EXE_CMD <= `EXE_ADD; WB_EN <= 1; Is_Imm <= 1; ST_or_BNE <= 1; MEM_R_EN <= 1; end
         `OP_ST: begin EXE_CMD <= `EXE_ADD; Is_Imm <= 1; MEM_W_EN <= 1; ST_or_BNE <= 1; end
+        
         // branch operations
         `OP_BEZ: begin EXE_CMD <= `EXE_NO_OPERATION; Is_Imm <= 1; Branch_command <= `COND_BEZ; branchEn <= 1; end
         `OP_BNE: begin EXE_CMD <= `EXE_NO_OPERATION; Is_Imm <= 1; Branch_command <= `COND_BNE; branchEn <= 1; ST_or_BNE <= 1; end
