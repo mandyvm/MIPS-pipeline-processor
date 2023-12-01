adc5a2514f7e58d9e00561f3d6778fcb90fdff94
Isabela Scarabelli <73960096+isascarabelli@users.noreply.github.com>
2023-12-01 19:11:32 -0300

    Update conditionChecker.v

--- modules/controlUnit/conditionChecker.v
+++ modules/controlUnit/conditionChecker.v
@@ -7,9 +7,16 @@ module conditionChecker (reg1, reg2, cuBranchComm, brCond);
 
   always @ ( * ) begin
     case (cuBranchComm)
-      `COND_JUMP: brCond <= 1;
-      `COND_BEZ: brCond <= (reg1 == 0) ? 1 : 0;
-      `COND_BNE: brCond <= (reg1 != reg2) ? 1 : 0;
+    `define COND_SLTU 4'b1001
+    `define COND_SLTIU 4'b1000
+    `define COND_SLTI 4'b0111
+    `define COND_SLT 4'b0110
+    `define COND_JR 4'b0101
+    `define COND_JAL 4'b0100
+    `define COND_JUMP 4'b0010
+    `define COND_BEQ 4'b0011
+    `define COND_BNE 4'b0001
+    `define COND_NOTHING 4'b0000
       default: brCond <= 0;
     endcase
   end
