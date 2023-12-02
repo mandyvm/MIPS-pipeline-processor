2fe9916889edc274c2aa32e6b912804eb78c59a6
Amanda VM <99031509+mandyvm@users.noreply.github.com>
2023-12-02 16:48:55 -0300

    Update ALU.v

--- modules/ALU.v
+++ modules/ALU.v
@@ -18,6 +18,27 @@ module ALU (val1, val2, EXE_CMD, aluOut);
       `EXE_SRA: aluOut <= val1 >> val2;
       `EXE_SRL: aluOut <= val1 >>> val2;
       `EXE_MULT: aluOut <= val1 * val2;
+      `EXE_MFHI: aluOut <= HI; // AVM
+      `EXE_MFLO: aluOut <= LO; // AVM
+      default: aluOut <= 0;
+    endcase
+  end
+
+  // Lógica para MFHI e MFLO AVM
+  always @ (posedge clock or posedge reset) begin
+    if (reset) begin
+      HI <= 0;
+      LO <= 0;
+    end else begin
+      if (MFHI) begin
+        HI <= aluOut;
+      end
+      if (MFLO) begin
+        LO <= aluOut;
+      end
+    end
+  end
+endmodule // ALU
       default: aluOut <= 0;
     endcase
   end
